5cb3c0c6506d7d470c18a4dbd205ca4f422a29ba
Luke Shaw <garbledman@gmail.com>
2019-10-25 18:13:46 +0200

    Added PING library (See Document for URL) to project to use as an alternative to a place picker from Google. Due to Google no longer supporting place picker an alternative had to be used. This option worked and was simple to implement. Also started to implement basic SQL for location saving, next step to save on location picked.

--- app/src/main/java/com/example/geoshoppingfinal/DataBaseHelper.java
+++ app/src/main/java/com/example/geoshoppingfinal/DataBaseHelper.java
@@ -4,18 +4,19 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.SyncStateContract;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
     //This is a helper class for basic creation of the Sqllite database
     //Code is based off this https://camposha.info/android-swipe-tabs-sqlite-fragments-with-listview/
 
     //SQlLite Create statement
-    private static final String CREATE_TABLE = "CREATE TABLE List(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, quantity INTEGER NOT NULL, bought INTEGER DEFAULT 0);";
+    private Context context;
 
     // Constructor
-    public DataBaseHelper(Context context) {
+    public DataBaseHelper(Context context)
+    {
         super(context, "geo_DB", null, 1);
+        this.context = context;
     }
 
     //CREATE TABLE
@@ -24,7 +25,8 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         try
         {
-            db.execSQL(CREATE_TABLE);
+            db.execSQL(context.getResources().getString(R.string.CREATE_TABLE_ITEM));
+            db.execSQL(context.getResources().getString(R.string.CREATE_TABLE_LOCATION));
         }catch (SQLException e)
         {
             e.printStackTrace();
@@ -37,7 +39,9 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         try {
             db.execSQL("DROP TABLE IF EXISTS List");
-            db.execSQL(CREATE_TABLE);
+            db.execSQL(context.getResources().getString(R.string.CREATE_TABLE_ITEM));
+            db.execSQL("DROP TABLE IF EXISTS Location");
+            db.execSQL(context.getResources().getString(R.string.CREATE_TABLE_LOCATION));
 
         }catch (SQLException e)
         {
--- app/src/main/java/com/example/geoshoppingfinal/MainActivity.java
+++ app/src/main/java/com/example/geoshoppingfinal/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.geoshoppingfinal;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import com.example.geoshoppingfinal.ui.home.HomeFragment;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.libraries.places.api.model.Place;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.rtchagas.pingplacepicker.PingPlacePicker;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -23,10 +35,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    //implements NavigationView.OnNavigationItemSelectedListener {
     private AppBarConfiguration mAppBarConfiguration;
+    private NavController navController;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -43,9 +58,50 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.nav_gallery){
+                    showPlacePicker();
+                }
+
+                boolean result = NavigationUI.onNavDestinationSelected(menuItem, navController);
+                drawer.closeDrawers();
+                return result;
+            }
+        });
+    }
+
+    private void showPlacePicker() {
+        PingPlacePicker.IntentBuilder builder = new PingPlacePicker.IntentBuilder();
+        builder.setAndroidApiKey("AIzaSyAaOSozlNmzZcjQ_8rgnSiZ1CX92wuskus")
+                .setMapsApiKey("AIzaSyDaDQRoc087nDE5_vfvwOGyYGCNRhNH4s4");
+
+        // If you want to set a initial location rather then the current device location.
+        // NOTE: enable_nearby_search MUST be true.
+        // builder.setLatLng(new LatLng(37.4219999, -122.0862462))
+
+        try {
+            Intent placeIntent = builder.build(this);
+            startActivityForResult(placeIntent, 1);
+        }
+        catch (Exception ex) {
+            Toast.makeText(this, "Google Play services is not available...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == 1) && (resultCode == RESULT_OK)) {
+            Place place = PingPlacePicker.getPlace(data);
+            if (place != null) {
+                Toast.makeText(this, "You selected the place: " + place.getName(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
